6417bed41e64f476fa0d5b305ce9b461ac07574c
Antonio Mateus <uidesign@fractalbases.com>
2022-09-17 22:20:13 -0300

    update

--- components/form.tsx
+++ components/form.tsx
@@ -176,7 +176,7 @@ export default function Form({ sharePage }: Props) {
             onChange={e => setEmail(e.target.value)}
             onFocus={() => setFocused(true)}
             onBlur={() => setFocused(false)}
-            placeholder="Digit seu email aqui"
+            placeholder=""
             aria-label="Your email address"
             required
           />
